5a0620ec6b36dcfbcafcd6e23af325f81750e512
Norman Aranez <araneznorman12@gmail.com>
2025-09-20 14:37:35 +0800

    add sample test

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Return the sum of x and y."""
+    return x + y
+
+
+def subtract(x, y):
+    """Return the subtraction of y from x."""
+    return x - y
